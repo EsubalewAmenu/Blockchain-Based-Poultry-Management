d77c92fee09ed00601e7a283e25a0545d0dcd3b7
Esubalew <esubalew.a2009@gmail.com>
2024-12-10 09:59:59 +0300

    dns validation removed

--- apps/accounts/validators.py
+++ apps/accounts/validators.py
@@ -54,33 +54,6 @@ import re
 def validate_email(email):
     email_regex = r'^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$'
     if not re.match(email_regex, email):
-        raise serializers.ValidationError({"error":"Invalid email format.", "status_code":400})
-
-    domain = email.split('@')[1]
-    try:
-        mx_records = dns.resolver.resolve(domain, 'MX')
-        mx_records = [str(mx_record.exchange) for mx_record in mx_records]
-    except (dns.resolver.NoAnswer, dns.resolver.NXDOMAIN):
-        return False
-
-    if not verify_email_account(email, mx_records):
-        return False  
-
+        raise serializers.ValidationError({"error": "Invalid email format.", "status_code": 400})
+    
     return email
-
-def verify_email_account(email, mx_records):
-    for mx_record in mx_records:
-        try:
-            server = smtplib.SMTP(mx_record)
-            server.set_debuglevel(0)
-            server.ehlo()
-            server.mail(settings.DEFAULT_FROM_EMAIL)
-            code, _ = server.rcpt(email)
-            
-            if code == 250:
-                return True
-        except Exception as e:
-            print(f"An error occurred during SMTP verification with {mx_record}: {e}")
-            return False
-
-    return False
